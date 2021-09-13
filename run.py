7012b7b1b94c8cf0b6c8ad3b639475aac5c36d07
tomasz.pionka <reports.poland@kinesso.com>
2021-09-13 18:01:17 +0200

    Added orchestrating script

---
+++ run.py
@@ -0,0 +1,9 @@
+import adform_weekly_report
+import adobe_weekly_report
+import facebook_weekly_report
+import google_analytics_weekly_report
+
+adform_weekly_report.main(scope="bmw")
+adobe_weekly_report.main()
+facebook_weekly_report.main()
+google_analytics_weekly_report.main()
