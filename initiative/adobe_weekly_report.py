187233b0259a03b2316c06b5df35d405e046094b
tomasz.pionka <reports.poland@kinesso.com>
2021-09-20 15:57:37 +0200

    Reformatted file

--- initiative/adobe_weekly_report.py
+++ initiative/adobe_weekly_report.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
+import functions
+
 campaign = pd.read_csv("adobe_files/adobe_campaign.csv")
 organic = pd.read_csv("adobe_files/adobe_organic.csv")
 sources = pd.read_csv("adobe_files/adobe_sources.csv")
-reports = [campaign,organic,sources]
-import functions
+reports = [campaign, organic, sources]
+
 tables = ['adobe_campaign_traffic', 'adobe_organic_traffic', 'new_adobe_sources']
 
+
 def main() -> object:
-    for report, table in zip(reports,tables):
+    for report, table in zip(reports, tables):
         functions.upload_data("bmw", table, report)
+    return True
