7ddfe472a0de075762353278d348db5e53c654d1
tomasz.pionka <reports.poland@kinesso.com>
2021-10-15 21:21:23 +0200

    Added scripts orchestrating server data flow

--- initiative/run.py
+++ initiative/run.py
@@ -1,13 +1,7 @@
 import adform_weekly_report
-import adobe_weekly_report
 import facebook_weekly_report
 import google_analytics_weekly_report
-import transcom_weekly_report
 
-# TODO fix adform requests logic
 adform_weekly_report.main(scope="bmw")
-adobe_weekly_report.main()
 facebook_weekly_report.main()
 google_analytics_weekly_report.main()
-transcom_weekly_report.main()
-
