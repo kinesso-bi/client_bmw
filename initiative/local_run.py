13b7546043d272ef21d84b6a8a41a1644608ecc1
tomasz.pionka <reports.poland@kinesso.com>
2021-10-15 21:20:28 +0200

    Added scripts orchestrating local machine data flow

---
+++ initiative/local_run.py
@@ -0,0 +1,7 @@
+import adobe_weekly_report
+import transcom_weekly_report
+import adobe_campaigns_weekly_report
+
+adobe_weekly_report.main()
+transcom_weekly_report.main()
+adobe_campaigns_weekly_report.main()
